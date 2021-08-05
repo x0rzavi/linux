9c7d0102aa650f68de3e64e4df3d72b77456b82a
Avishek Sen <x0rzavi@gmail.com>
2021-08-05 18:10:55 +0530

    Add GCC Clocks, Resets, GDSCs

---
+++ include/dt-bindings/reset/qcom,gcc-msm8917.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright 2021 Avishek Sen <x0rzavi@gmail.com>
+ */
+
+#ifndef _DT_BINDINGS_RESET_MSM_GCC_8917_H
+#define _DT_BINDINGS_RESET_MSM_GCC_8917_H
+
+/* Indexes for GCC Resets */
+#define GCC_CAMSS_MICRO_BCR             0
+#define GCC_USB_FS_BCR                  1
+#define GCC_USB_HS_BCR                  2
+#define GCC_USB2_HS_PHY_ONLY_BCR        3
+#define GCC_QUSB2_PHY_BCR               4
+
+#endif
